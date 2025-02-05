af1d0ee1a43fb900d480b8efec9bf9abff4e4650
Scarlet <70824102+scarletquasar@users.noreply.github.com>
2025-02-05 17:39:08 -0300

    feat: add UNDERSTAND_LANGUAGES constant to support multilingual functionality

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 const BRIDGE_LOCATION = './lib';
 const BRIDGE_TTL = 5000;
 const AUTOMATION_NAMES = ['cmoon', 'c-moon', 'see moon'];
+const UNDERSTAND_LANGUAGES = ['english', 'japanese', 'portuguese'];
 
-export { BRIDGE_LOCATION, BRIDGE_TTL, AUTOMATION_NAMES };
+export { BRIDGE_LOCATION, BRIDGE_TTL, AUTOMATION_NAMES, UNDERSTAND_LANGUAGES };
