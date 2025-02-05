80e5b498b1585ea71f439d3fe9e2b3bd7e65489e
Scarlet <70824102+scarletquasar@users.noreply.github.com>
2025-02-05 13:51:01 -0300

    feat: add new pre-processing actions for retrieving system and browser information

--- src/models/preProcessingActions.ts
+++ src/models/preProcessingActions.ts
@@ -2,6 +2,10 @@ enum PreProcessingActions {
     ConvertTextToAudio,
     ConvertAudioToText,
     GetVideoExpressionsContext,
+    GetCurrentOpenProcesses, // Operating System
+    GetCurrentOpenBrowserTabs, // Browser
+    GetCurrentOpenFolders, // Operating System
+    GetCurrentGitBranchesOpened, // Git
 }
 
 export { PreProcessingActions };
