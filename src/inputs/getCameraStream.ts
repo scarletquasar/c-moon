29108055a701fa93204c240b3168da033d4e03eb
Scarlet <70824102+scarletquasar@users.noreply.github.com>
2025-02-05 17:22:11 -0300

    feat: add getCameraStream function using node-webcam; update package.json and package-lock.json with new dependencies

--- src/inputs/getCameraStream.ts
+++ src/inputs/getCameraStream.ts
@@ -1,3 +1,43 @@
-async function getCameraStream() {}
+import { randomUUID } from 'crypto';
+import NodeWebcam from 'node-webcam';
+import { PassThrough } from 'stream';
+
+const opts = {
+    width: 1280,
+    height: 720,
+    quality: 100,
+    frames: 60,
+    delay: 0,
+    saveShots: true,
+    output: 'jpeg',
+    device: false,
+    callbackReturn: 'location',
+    verbose: true,
+} as const;
+
+async function getCameraStream(): Promise<NodeJS.WritableStream> {
+    var webcam = NodeWebcam.create(opts);
+    await new Promise<void>((resolve, reject) => {
+        const interval = setInterval(() => {
+            webcam.capture(`${randomUUID().replaceAll('-', '')}`, function (err: any, data: any) {
+                if (err) {
+                    clearInterval(interval);
+                    reject(err);
+                    return;
+                }
+            });
+        }, 1000 / opts.frames);
+
+        setTimeout(() => {
+            clearInterval(interval);
+            resolve();
+        }, 3000);
+    });
+    const bufferArray = webcam.shots.map((shot) => shot.data);
+    const stream = new PassThrough();
+    bufferArray.forEach((buffer) => stream.write(buffer));
+    stream.end();
+    return stream;
+}
 
 export { getCameraStream };
