bf65007ca46da76715fc28eece22740131d178c1
Scarlet <70824102+scarletquasar@users.noreply.github.com>
2025-02-05 17:02:34 -0300

    feat(wip): add microphone-stream dependency; introduce getMicrophoneRecursive function and update consts with automation names

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,5 @@
 const BRIDGE_LOCATION = './lib';
 const BRIDGE_TTL = 5000;
+const AUTOMATION_NAMES = ['cmoon', 'c-moon', 'see moon'];
 
-export { BRIDGE_LOCATION, BRIDGE_TTL };
+export { BRIDGE_LOCATION, BRIDGE_TTL, AUTOMATION_NAMES };
